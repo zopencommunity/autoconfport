7d5804c2cc956f9e62bc91df9305f50e17ae0c01
Mike Fulton <mikefultonpersonal@gmail.com>
2022-05-19 18:34:08 -0400

    remove autoscan and use hand-written scan

--- tests/basic/cleanup.sh
+++ tests/basic/cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Remove any previously generated files
-rm -f autoscan.log config.h config.h.in config.log config.status configure configure.ac hw.c hw.o hw configure.scan configure~
-rm -rf autom4te.cache
+#rm configure.scan
+rm -f autoscan.log config.h config.h.in config.log config.status configure configure.ac hw.c hw.o hw configure~
+rm -rf autom4te.cache config.cache
