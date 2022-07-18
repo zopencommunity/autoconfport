51d7c6f228258a14afe9be764fbd44593461f161
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:48 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'autoconfport'), string(name: 'DESCRIPTION', 'autoconfport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'autoconfport'), string(name: 'DESCRIPTION', 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' )]
         }
 }
