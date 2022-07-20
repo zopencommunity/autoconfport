23788d05babbecbebf7c81a8d3f0c421ea7c9d12
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 10:00:38 -0400

    Add full repo url

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
 	stage('Build') {
-		build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'autoconfport'), string(name: 'PORT_DESCRIPTION', value: 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' )]
+		build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/autoconfport.git'), string(name: 'PORT_DESCRIPTION', value: 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' )]
 	}
 }
