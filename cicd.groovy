08b18fbcfec067fd878e53ff12e7b11ef16d8d4b
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:22 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
 	stage('Build') {
-		build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'autoconfport'), string(name: 'DESCRIPTION', 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' )]
+		build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'autoconfport'), string(name: 'DESCRIPTION', value: 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' )]
 	}
 }
