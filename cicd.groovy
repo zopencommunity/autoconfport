63f6bdf7ae95071670414f5c90dcd0bd6fd48794
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:10 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
 			branches: [[name: '*/main']],
  			doGenerateSubmoduleConfigurations: false, 
 			extensions: [], 
-			userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/autoconfport.git']]])
+			userRemoteConfigs: [[url: 'https://github.com/zopencommunity/autoconfport.git']]])
         }
 
 	stage('Build') {
-		build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/autoconfport.git'), string(name: 'PORT_DESCRIPTION', value: 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' ),  string(name: 'NODE_LABEL', value: "v2r4")]
+		build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/autoconfport.git'), string(name: 'PORT_DESCRIPTION', value: 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.' ),  string(name: 'NODE_LABEL', value: "v2r4")]
 	}
 }
